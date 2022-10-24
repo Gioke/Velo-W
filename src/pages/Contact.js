bbca8594353494dace3bd096df3903c315f032d2
Giovanni Overmeire <101796485+Gioke@users.noreply.github.com>
2022-10-24 12:07:43 +0200

    contact form styled

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,14 +4,18 @@ import '../styles/Contact.css';
 export const Contact = () => {
     return (
         <>
-            <h1 className="font-extrabold text-3xl flex justify-center underline decoration-amber-500">Contact Me:</h1>
+            <h1 className="contact-title">Contact Me:</h1>
 
-            <form className="h-96 grid grid-cols-3 gap-4 content-center">
-                <input placeholder="Naam" className="border-solid border-2 border-gray-400 h-8 w-64 ml-10"></input>
-                <input placeholder="E-mail" className="border-solid border-2 border-gray-400"></input>
-                <input placeholder="Telefoon" className="border-solid border-2 border-gray-400 w-64 ml-10"></input>
-                <textarea placeholder="Text" className="border-solid border-2 border-gray-400 col-start-2 h-64"></textarea>
-                <button className="border-solid border-2 border-gray-400 col-start-2 rounded-lg">Send</button>
+            <form className="form-container">
+                <div className="contact-info">
+                    <input placeholder="Naam" className="form-naam"></input>
+                    <input placeholder="E-mail" className="form-mail"></input>
+                    <input placeholder="Telefoon" className="form-telefoon"></input>
+                </div>
+                <textarea placeholder="Text" className="form-text"></textarea>
+                <div className="button-container">
+                    <button>Send</button>
+                </div>
             </form>
         </>
     )
